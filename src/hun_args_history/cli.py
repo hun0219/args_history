466e05a84a10dd1ad1dfc3bed7a52d7082d2be73
hun0219 <gsw98000@gmail.com>
2024-07-25 14:50:43 +0900

    TOP10 note 추가

--- src/hun_args_history/cli.py
+++ src/hun_args_history/cli.py
@@ -23,15 +23,14 @@ def cmd():
 
     if args.scount:
         print(f"-s => {args.scount}")
-        # TODO 멸령어 카운트
+        # TODO 명령어 카운트
     elif args.top:
         print(f"-t => {args.top}")
         if args.dt:
             print(f"-d => {args.dt}")
             # TODO 특정 날짜의 명령어 TOP N
         else:
-            print("TODO - 에러 안내 메시지를 주면")
-            #parser.print_help()
+            #print("TODO - 에러 안내 메시지를 주면")
             parser.error("-t 옵션은 -d 옵션과 함께 사용하세요")
     else:
         # TODO - 사용법을 출력한다
