12dbc93b23e88b00f51f51a74f885d664bd61d64
hun0219 <gsw98000@gmail.com>
2024-07-26 09:14:21 +0900

    add parse_args

--- src/hun_args_history/cli.py
+++ src/hun_args_history/cli.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 from hun_args_history.db.utils import count, top, read_data
+from tabulate import tabulate
 
 
 def hello_msg():
@@ -19,9 +20,10 @@ def cmd():
     parser.add_argument('-s', '--scount') 
     parser.add_argument('-t', '--top', type=int)
     parser.add_argument('-d', '--dt')
+    parser.add_argument('-p', '--pret', action='store_true')
     
     args = parser.parse_args()
-    print(args.scount, args.top, args.dt)
+    print(args.scount, args.top, args.dt, args.pret)
 
     if args.scount:
         # TODO 명령어 카운트
@@ -33,8 +35,14 @@ def cmd():
         if args.dt:
             # TODO 특정 날짜의 명령어 TOP N
             # print(f"-d => {args.dt}")
+            #t = top(args.top, args.dt, args.pret )
+            #print(tabulate(t, tablefmt="outline" ))
             t = top(args.top, args.dt)
-            print(t)
+            if args.pret:
+                t = top(args.top, args.dt, args.pret)
+                print(t)
+            else:
+                print(t)
         else:
             #print("TODO - 에러 안내 메시지를 주면")
             parser.error("-t 옵션은 -d 옵션과 함께 사용하세요")
--- src/hun_args_history/db/utils.py
+++ src/hun_args_history/db/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from tabulate import tabulate
 
 def read_data(path='~/data/parquet'):
     df = pd.read_parquet(path)
@@ -6,17 +7,24 @@ def read_data(path='~/data/parquet'):
     #df = df.dropna()
     return df
 
-def top(cnt, dt):
+def top(cnt, dt, pretty=False):
     df = read_data()
     fdf = df[df['dt'] == dt]
     sdf = fdf.sort_values(by='cnt', ascending=False).head(cnt)
     ddf = sdf.drop(columns=['dt'])
-
-    r = (ddf.to_string(index=False))
-    return r
+    pty = tabulate(ddf, headers=["cmd","cnt"], tablefmt="outline")
+    
+    if pretty:
+        # 참일떄
+        r = tabulate(ddf, headers=["cmd","cnt"], tablefmt="outline")
+        return r 
+    else:  #거짓 일떄 
+        r = ddf.to_string(index=False)
+        return r
 
 def count(query):
     df = read_data()
     fdf = df[df['cmd'].str.contains(query)]
     cnt = fdf['cnt'].sum()
     return cnt
+
